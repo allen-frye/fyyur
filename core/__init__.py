71712b22fbc793ece8b38cf37994762abda13f93
Allen <>
2022-10-23 21:19:35 -0500

    added citation for separation of concerns to dunder file.

--- core/__init__.py
+++ core/__init__.py
@@ -17,34 +17,4 @@ app.config['DEBUG'] = True
 
 import views
 
-
-
-# def create_app(test_config=None):
-#     # create and configure the app
-#     app = Flask(__name__, instance_relative_config=True)
-#     app.config.from_mapping(
-#         SECRET_KEY=os.urandom(32),
-#         DEBUG = True,
-#         # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-#     	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:plumbflo22!@localhost:5432/fyyur'
-
-#     )
-#     if test_config is None:
-#         # load the instance config, if it exists, when not testing
-#         app.config.from_pyfile('config.py', silent=True)
-#     else:
-#         # load the test config if passed in
-#         app.config.from_mapping(test_config)
-
-#     # ensure the instance folder exists
-#     try:
-#         os.makedirs(app.instance_path)
-#     except OSError:
-#         pass
-
-#     # a simple page that says hello
-#     @app.route('/hello')
-#     def hello():
-#         return 'Hello, World!'
-
-#     return app
+# For separation of concerns, I followed the approach in this article: https://dev.to/curiouspaul1/creating-modularized-flask-apps-with-blueprints-19nc
